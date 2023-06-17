698ad4efac8b8f8cd8c16d5d8164ea334c094402
suryahk <tejaksurya@gmail.com>
2023-06-17 18:24:09 +0530

    testing done

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // express
 let express = require("express");
 let app = express();
-
+/*
 app.get('/' , (req,res)=>{
 	res.status.json({data : 'running on port 3002'});
 });
 
 app.listen(3002 , ()=>{});
 
+*/
 
+console.log(30);
 
