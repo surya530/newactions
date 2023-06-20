abd43e3f0acb3763da30d6346a846297f147c5e8
suryahk <tejaksurya@gmail.com>
2023-06-20 07:42:14 +0530

    new commit

---
+++ data.js
@@ -0,0 +1,11 @@
+
+let data = [
+	{ 'name' : 'surya' , 'age' : 24 }, 
+	{ 'name' : 'teja' , 'age' : 24 }, 
+	{ 'name' : 'natsu' , 'age' : 100 }, 
+   ];
+
+module.exports = data;
+
+
+
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // express
 let express = require("express");
+let cors = require('cors');
+let path = require("path");
+let data = require("./data.js");
 let app = express();
-/*
-app.get('/' , (req,res)=>{
-	res.status.json({data : 'running on port 3002'});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors({ origin : '*' }));
+
+/*app.use(express.static(path.join(__dirname , 'build')));
+
+app.get('/' , async (req, res)=>{
+	res.sendFile(path.join(__dirname , 'build' , 'index.html'));
+});
+*/
+app.get('/data' , (req,res)=>{
+	res.status(200).json({data : data});
 });
 
 app.listen(3002 , ()=>{});
 
-*/
-
-console.log(30);
+//console.log({data});
 
