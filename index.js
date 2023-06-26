76bc82641fdaece56d016afa396c05a9b257884b
suryahk <tejaksurya@gmail.com>
2023-06-26 23:21:22 +0530

    rest in peace

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors({ origin : '*' }));
 
-/*app.use(express.static(path.join(__dirname , 'build')));
+app.use(express.static(path.join(__dirname , 'build')));
 
 app.get('/' , async (req, res)=>{
 	res.sendFile(path.join(__dirname , 'build' , 'index.html'));
 });
-*/
+
 // console.log("surya");
 app.get('/data' , (req,res)=>{
 	res.status(200).json({data : data});
 });
 
-app.listen(3002 , ()=>{});
+app.listen(4004 , ()=>{});
 
 // // console.log({data});
 
